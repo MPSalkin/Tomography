f85a4e16e958b6a712a60feb9bd392ba5946a96b
MPSalkin <mpsalkin@csu.fullerton.edu>
2017-03-06 21:28:31 -0800

    Updated for removing multiple subsets

--- RemoveSubsets.py
+++ RemoveSubsets.py
@@ -1,20 +1,23 @@
-
 import os.path
 import numpy as np
 
 # Prompt user for input
 Iterations = raw_input("How many iterations is the file? \n")
-Subsets = raw_input("What is the number of subsets to be removed? \n")
+NumTot = int(raw_input('Enter the number of groups of subsets to be removed. \n'))
+Subsets = [None]*(NumTot)
+for i in range(0,(NumTot)):
+	Subsets[i] = raw_input( str(i) + ": Enter subset value to be removed? \n")
 
 # Open file for specified iterations
-fname = 'Visuals/Data/Objective_Function_data_' + (Iterations) + 'Iterations.npy'
+fname = 'Visuals/Data/Objective_Function_data_' + Iterations + 'Iterations.npy'
 Data = np.load(fname)
 Data = Data[()]
 
 # Delete subsets from dictionary
-del Data['Itr'+Subsets]
-del Data['SSIM'+Subsets]
-del Data['Time'+Subsets]
+for i in range(0,(NumTot)):
+	del Data['Itr'+Subsets[i]]
+	del Data['SSIM'+Subsets[i]]
+	del Data['Time'+Subsets[i]]
 
 # Resave the data
 np.save(fname, Data) 
